8890219898c73f044fc221aadf76ea9192b8200d
MrDuckVC <cunev2005@gmail.com>
2025-08-26 06:12:50 -0400

    Final Project Done

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2  # Number of blank choices shown
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2  # Number of blank questions shown
 
 
 class LessonInline(admin.StackedInline):
@@ -22,9 +30,15 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
